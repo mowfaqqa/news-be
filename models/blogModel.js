56d4b3f804b6340173cf62375719170659ce1966
muwaffaq <itzmuwaffaq@gmail.com>
2024-11-27 17:38:58 +0100

    fixes

--- models/blogModel.js
+++ models/blogModel.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const blogSchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
-  author: { type: String, required: true },
   date: { type: Date, default: Date.now },
 });
 
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -10,10 +10,10 @@ const authMiddleware = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.post("/", authMiddleware, createBlog);
-router.get("/", getAllBlogs); // Public access to view blogs
-router.get("/:id", getBlogById); // Public access to view a blog by ID
-router.put("/:id", authMiddleware, updateBlog);
-router.delete("/:id", authMiddleware, deleteBlog);
+router.post("/create", authMiddleware, createBlog);
+router.get("/blog", getAllBlogs); 
+router.get("//blog/:id", getBlogById);
+router.put("/blog/:id", authMiddleware, updateBlog);
+router.delete("/blog/:id", authMiddleware, deleteBlog);
 
 module.exports = router;
